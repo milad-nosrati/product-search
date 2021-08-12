268a59bc21fdabfb6d49794adc703a42501b38ab
Milad Nosrati <milad.nosrati@gmail.com>
2021-08-12 17:40:43 +1000

    UI bugs fixed

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -6,7 +6,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
-
+        
         this.state = {
             searchQuery: "",
             products: [],
@@ -20,32 +20,47 @@ export default class Main extends React.Component {
             displayResult: false,
             ExtURL: '',
             Unitex: false,
+            alertMessage: ''
         }
+    }
+
+    handleResult = (status) => {
+        this.setState({
+            displayResult: status
+        }, this.handleLoading(!status));
     }
     handleLoading = (status) => {
         this.setState({
             loadingState: status
         })
     }
-    handleResult = (status) => {
-        this.setState({
-            displayResult: status
-        }, this.handleLoading(!status));
-    }
     handleChange = (e) => {
         this.setState({ searchQuery: e.target.value });
     }
     handleSearch = (e) => {
-        this.handleResult(false);
-        this.setState({
-            products: []
-        });
-        const searchPhrase = 'products/?sku=' + this.state.searchQuery
-        API.get(searchPhrase)
+        if(this.state.searchQuery === ''){
+          alert("Please search SKU in the search box");
+          this.handleLoading(false);
+        }else{
+            this.handleResult(false);
+            this.setState({
+                products: []
+            });
+            
+            const searchPhrase = 'products/?sku=' + this.state.searchQuery
+            API.get(searchPhrase)
             .then(res => {
-                const results = res.data;
-                this.setState({ products: results[0] }, this.handleProduct(results[0]));
+                console.log(res)
+                if(res.data ===[]){
+                    alert("No result has been found, try another sku");
+
+                }else{
+                    const results = res.data;
+                    this.setState({ products: results[0] }, this.handleProduct(results[0]));
+                }
+                
             })
+        }        
     }
     handleProduct = (product) => {
         let ExtLink = '';
@@ -65,15 +80,15 @@ export default class Main extends React.Component {
             ExtURL: ExtLink,
             Unitex: unitexCheck,
         })
-
+        
         const searchParentPhrase = 'products/' + product.parent_id;
         API.get(searchParentPhrase)
-            .then(res => {
-                const parentResults = res.data;
-                this.setState({
-                    parent: parentResults
-                }, this.handleParent(parentResults));
-            })
+        .then(res => {
+            const parentResults = res.data;
+            this.setState({
+                parent: parentResults
+            }, this.handleParent(parentResults));
+        })
     }
     handleParent = (parent) => {
         const collectionName = parent.categories[0].name;
@@ -84,102 +99,103 @@ export default class Main extends React.Component {
             sizes: sizes,
         }, this.handleResult(true));
     }
-
+    
     render() {
         return (
-
+            
             <Container fluid id='main-wrapper' className="d-flex flex-column">
                 <Row className="justify-content-md-center">
                     <Col id="main-container" xl={5} lg={6} md={8} sm={10}  xs={12}>
-                        <Row className="d-flex flex-column">
+                        <Row className="d-flex flex-column ">
                             <Row>
                                 <Col>
                                     <img src='/images/logo.png'
-                                        id="main-logo"
-                                        alt='Payless Flooring logo' />
+                                    id="main-logo"
+                                    alt='Payless Flooring logo' />
                                 </Col>
                             </Row>
-                            <Row>
-                                <Col md={8} xs={12}>
+                            <Row className="d-flex flex-row align-items-center">
+                                <Col   sm ={9} xs={12}>
                                     <input type="text"
-                                        id="main-search-input"
-                                        placeholder=' Search SKU'
-                                        onChange={this.handleChange} />
+                                    id="main-search-input"
+                                    placeholder=' Search SKU'
+                                    onChange={this.handleChange} />
                                 </Col>
-                                <Col md={4} xs={12}>
+                                <Col  sm={3} xs={12}>
                                     <button id='main-search-button'
-                                        type="button"
-                                        className="btn btn-primary"
-                                        onClick={this.handleSearch}>
-                                        {this.state.loadingState ? 'Loading data..' : 'Search'}
+                                    type="button"
+                                    className="btn btn-primary "
+                                    onClick={this.handleSearch}>
+                                    {this.state.loadingState ? 'Loading data..' : 'Search'}
                                     </button>
                                 </Col>
                             </Row>
                             <Row>
                                 <Col id='loading' className={`${this.state.loadingState ? "Visible" : "Hidden"}`}>
-                                    <div class="loader"></div>
+                                    <div className="loader"></div>
                                     <span className="loading-label">
-                                        Waiting for the server response
+                                    Waiting for the server response
                                     </span>
                                 </Col>
                             </Row>
                         </Row>
-
+            
                         <Container className={`${this.state.displayResult ? "Visible" : "Hidden"} p-0`}>
+                            <hr />
                             <Row>
-                                <Col md={2} xs={12}>
+                                <Col md={2} sm={3} xs={12}>
                                     <img src={this.state.imageSource}
-                                        alt={this.state.imagesAlt}
-                                        className="product-image" />
+                                    alt={this.state.imagesAlt}
+                                    className="product-image" />
                                 </Col>
-                                <Col md={10} xs={12} className="text-left">
+                                <Col md={10} sm = {9} xs={12} className="text-left">
                                     <p className="p-xs-0 p-m-1">
                                         <span className="results-label">
-                                            Payless: 
+                                        Payless: 
                                         </span>
                                         <a href={this.state.products.permalink}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                            className="results-result">
-                                            {this.state.products.name}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="results-link">
+                                        {this.state.products.name}
                                         </a>
                                     </p>
                                     <p>
                                         <span className="results-label">
-                                            Collection:
+                                        Collection:
                                         </span>
                                         <span className="results-result">
-                                            {this.state.collection}
+                                        {this.state.collection}
+                                        </span>
+                                        </p>
+                                        <p>
+                                        <span className="results-label">
+                                        Available Stock: 
+                                        </span>
+                                        <span className="results-result">
+                                        {this.state.products.stock_quantity}
                                         </span>
                                     </p>
                                     <p>
                                         <span className="results-label">
-                                            Available Stock: 
+                                        Sale Price:
                                         </span>
                                         <span className="results-result">
-                                            {this.state.products.stock_quantity}
-                                        </span>
-                                    </p>
-                                    <p>
-                                        <span className="results-label">
-                                            Sale Price:
-                                        </span>
-                                        <span className="results-result">
-                                            ${this.state.products.sale_price}
+                                        ${this.state.products.sale_price}
                                         </span>
                                         <span className="results-label">
-                                           | RRP:
+                                        | RRP:
                                         </span>
-                                        <span className="results-result">
-                                            ${this.state.products.regular_price}
+                                            <span className="results-result">
+                                        ${this.state.products.regular_price}
                                         </span>
                                     </p>
                                     <p>
                                         <a href={this.state.ExtURL}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                            className="results-button">
-                                            Product on {this.state.Unitex ? ' Unitex ' : " Saray "} website
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="results-button">
+                                        Product on {this.state.Unitex ? ' Unitex ' : " Saray "} website
                                         </a>
                                     </p>
                                 </Col>
@@ -188,6 +204,7 @@ export default class Main extends React.Component {
                     </Col>
                 </Row>
             </Container>
-        );
+            );
+        }
     }
-}
+    
