aaa5003b29484ae7e2dcf67ccf96178dea2db56e
Milad Nosrati <milad.nosrati@gmail.com>
2021-08-16 14:35:26 +1000

    API fixewd - Netlify BUG

--- src/Auth/api.js
+++ src/Auth/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL,
+  // Netlify Fix: for some reason Netlify changed the BaseURL to netlify URL!!
+  baseURL: 'https://paylessflooring.com.au/wp-json/wc/v3/',
   auth: {
     username: process.env.REACT_APP_USERNAME,
     password: process.env.REACT_APP_PASSWORD
